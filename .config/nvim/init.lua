b4fc36ffafcd3a4619c61ba02195ae9d45bc4258
michael <michael.wang@valon.com>
2026-03-16 15:46:17 -0400

    Add gitsigns for inline git blame in Neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,16 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch=stable",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
@@ -5,3 +18,12 @@ vim.opt.termguicolors = true
 vim.cmd("syntax on")
 vim.cmd("filetype plugin indent on")
 vim.cmd.colorscheme("habamax")
+
+require("lazy").setup({
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
+    },
+  },
+})
